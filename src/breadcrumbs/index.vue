fe81444aabd49bf52a9f98e2209116109326cc42
TonGashi <mg.meriton@gmail.com>
2021-08-30 15:55:34 +0200

    updated version 0.0.23, initial commit for preventing to make payment, if clearance request pending

--- src/breadcrumbs/index.vue
+++ src/breadcrumbs/index.vue
@@ -1,5 +1,9 @@
 <template>
-	<div class="columns is-marginless">
+	<div class="columns is-multiline is-marginless">
+		<div v-if="false" class="column is-12 notification is-warning py-5 blink">
+			Payments are not allowed until your last 
+			<strong>Clearance Request</strong> will be Approved or request for Date adjust.
+		</div>	
 		<div class="column is-narrow" style="padding:0;padding-bottom:25px !important">
 			<nav class="breadcrumb">
 				<ul v-if="paths.length>0" class="is-narrow">
@@ -15,12 +19,38 @@
 	</div>
 </template>
 <script>
+	import { mapActions, mapGetters } from 'vuex';
 	export default {
 		name: 'Breadcrumbs',
 		data() { return { paths: [] } },
 		beforeMount() { this.updateBreadcrumbs() },
-		computed: { hasBreadcrumbsRight() { return !!this.$slots["breadcrumbs-right"] } },
+		mounted() {
+			this.checkIfCanSell();
+		},
+		computed: { 
+			...mapGetters("canvasOffcanvas", ["cannotSell"]),
+			hasBreadcrumbsRight() { return !!this.$slots["breadcrumbs-right"] }
+		},
 		methods: {
+			...mapActions("canvasOffcanvas", ["stopSelling", "startSelling"]),
+			checkIfCanSell() {
+				this.database()
+					.ref("clearancerequests", "accounting")
+					.where([{field: "user.value", value: this.currentUser.id}])
+					.with("specification")
+					.paginate(1, 1, true)
+					// .with("specification,user")
+					.once()
+					.then((response) => {
+						if(response.data) {
+							console.log(response.data.data[0]);
+							console.log(response.data.data[0].specification.end_date>=this.dayjs().format("YYYY-MM-DD"));
+							if(response.data.data[0].specification.end_date>=this.dayjs().format("YYYY-MM-DD")) {	
+								this.stopSelling();
+							}
+						}
+					})
+			},
 			updateBreadcrumbs: function() {
 				this.paths = [];
 				let paths = this.$route.path.substring(1).split("?").shift().split("/");
@@ -38,3 +68,18 @@
 		}
 	}
 </script>
+<style scoped>
+	@keyframes blink {  
+	  0% { background-color: #ffdd57; }
+	  100% { background-color: black; }
+	}
+	@-webkit-keyframes blink {
+	  0% { background-color: #ffdd57; }
+	  100% { background-color: black; }
+	}
+	.blink {
+	  -webkit-animation: blink 0.7s linear infinite;
+	  -moz-animation: blink 0.7s linear infinite;
+	  animation: blink 0.7s linear infinite;
+	} 
+</style>
